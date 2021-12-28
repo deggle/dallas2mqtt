3dfa74dce819856dc74a2b7b2c6e533eb3f4aa20
Tim Alston <tim@deggle.com>
2021-12-28 11:41:48 +0000

    First pass.

--- src/EnvironmentalMonitoring.ino
+++ src/EnvironmentalMonitoring.ino
@@ -3,12 +3,11 @@ extern "C" {
   #include "freertos/FreeRTOS.h"
   #include "freertos/timers.h"
 }
+
+#include <Secrets.h>
 #include <AsyncMqttClient.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-#define WIFI_SSID "CH-Devices"
-#define WIFI_PASSWORD "CH*Devices"
 
 //#define MQTT_HOST "iot.preventx.net"
 #define MQTT_HOST "10.0.3.1"
@@ -18,9 +17,11 @@ extern "C" {
 #define ONE_WIRE_BUS 26
 #define TEMPERATURE_PRECISION 9
 
+#define BUTTON_PIN 39
+
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-float temp;
+int numberOfDevices;
 
 AsyncMqttClient mqttClient;
 TimerHandle_t mqttReconnectTimer;
@@ -88,13 +89,57 @@ void onMqttPublish(uint16_t packetId) {
   Serial.println(packetId);
 }
 
+void loopDevices() {
+
+
+    
+    Serial.println("Searching for 1-wire devices...");
+
+
+
+
+  // Grab a count of devices on the wire
+    numberOfDevices = sensors.getDeviceCount();
+
+    Serial.print("Found ");
+    Serial.print(numberOfDevices, DEC);
+    Serial.println(" devices.");
+
+
+    // // Search for connected devices...
+    // uint8_t address[8];
+    // uint8_t count = 0;
+
+    // if (oneWire.search(address)) {
+      
+    //   do {
+    //     count++;
+    //     Serial.println("  {");
+    //     for (uint8_t i = 0; i < 8; i++)
+    //     {
+    //       if (address[i] < 0x10) Serial.print("0");
+    //       Serial.print(address[i], HEX);
+    //     }
+    //     Serial.println("...");
+    //   } while (oneWire.search(address));
+
+    //   Serial.println(" # ");
+    //   Serial.print("// nr devices found: ");
+    //   Serial.println(count);
+
+
+      
+    // } else {
+    //   Serial.println("No devices found.");
+    // }
+    
+}
+
 void setup() {
-  // Start the DS18B20 sensor
-  sensors.begin();
-  
+ 
   Serial.begin(115200);
-  Serial.println();
-  Serial.println();
+
+  connectToWifi();
 
   mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
   wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
@@ -103,62 +148,38 @@ void setup() {
 
   mqttClient.onConnect(onMqttConnect);
   mqttClient.onDisconnect(onMqttDisconnect);
-  //mqttClient.onSubscribe(onMqttSubscribe);
-  //mqttClient.onUnsubscribe(onMqttUnsubscribe);
+
   mqttClient.onPublish(onMqttPublish);
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-  // If your broker requires authentication (username and password), set them below
-  //mqttClient.setCredentials("REPlACE_WITH_YOUR_USER", "REPLACE_WITH_YOUR_PASSWORD");
-  
-  
-  // Search for connected devices...
-  uint8_t address[8];
+  //mqttClient.setCredentials("USER", "PASSW");
+   
+  // Start the DS18B20 sensor
+  sensors.begin();
+  loopDevices();
 
-  if (oneWire.search(address)) {
-    uint8_t count = 0;
-    do {
-      count++;
-      Serial.println("  {");
-      for (uint8_t i = 0; i < 8; i++)
-      {
-        if (address[i] < 0x10) Serial.print("0");
-        Serial.print(address[i], HEX);
-      }
-      Serial.println("...");
-    } while (oneWire.search(address));
-
-    Serial.println(" # ");
-    Serial.print("// nr devices found: ");
-    Serial.println(count);
-
-
-    
-  } else {
-    Serial.println("No devices found.");
-  }
-  
-  connectToWifi();
   
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  // Every X number of seconds (interval = 10 seconds) 
-  // it publishes a new MQTT message
-  if (currentMillis - previousMillis >= interval) {
-    // Save the last time a new reading was published
-    previousMillis = currentMillis;
-    // New temperature readings
-    sensors.requestTemperatures(); 
-    // Temperature in Celsius degrees
-    temp = sensors.getTempCByIndex(0);
-    // Temperature in Fahrenheit degrees
-    //temp = sensors.getTempFByIndex(0);
+ 
+  // unsigned long currentMillis = millis();
+  // // Every X number of seconds (interval = 10 seconds) 
+  // // it publishes a new MQTT message
+
+  // if (currentMillis - previousMillis >= interval) {
+  //   // Save the last time a new reading was published
+  //   previousMillis = currentMillis;
+  //   // New temperature readings
+  //   sensors.requestTemperatures(); 
+  //   // Temperature in Celsius degrees
+  //   temp = sensors.getTempCByIndex(0);
     
-    // Publish an MQTT message on topic esp32/ds18b20/temperature
-    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PREFIX, 1, true, String(temp).c_str());                            
-    Serial.printf("Publishing on topic %s at QoS 1, packetId: ", MQTT_PREFIX);
-    Serial.println(packetIdPub1);
-    Serial.printf("Message: %.2f /n", sensors.getTempCByIndex(0));
-  }
+  //   // Publish an MQTT message on topic esp32/ds18b20/temperature
+  //   uint16_t packetIdPub1 = mqttClient.publish(MQTT_PREFIX, 1, true, String(temp).c_str());                            
+  //   Serial.printf("Publishing on topic %s at QoS 1, packetId: ", MQTT_PREFIX);
+  //   Serial.println(packetIdPub1);
+  //   Serial.printf("Message: %.2f \n", sensors.getTempCByIndex(0));
+
+  // }
+
 }
