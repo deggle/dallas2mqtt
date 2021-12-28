531730bdd8f82ea39eb8fcb4c20a5c9cffad50a5
Tim Alston <tim@deggle.com>
2021-12-28 16:05:30 +0000

    First complete code.

--- src/EnvironmentalMonitoring.ino
+++ src/EnvironmentalMonitoring.ino
@@ -8,49 +8,52 @@ extern "C" {
 #include <AsyncMqttClient.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-//#define MQTT_HOST "iot.preventx.net"
-#define MQTT_HOST "10.0.3.1"
-#define MQTT_PORT 1883
-#define MQTT_PREFIX "px/temperature/"
+#include <Button2.h>
+#include <FastLED.h> 
 
 #define ONE_WIRE_BUS 26
-#define TEMPERATURE_PRECISION 9
+#define TEMPERATURE_PRECISION 12
+
+#define PIN_LEDATOM 27
+CRGB ledAtom[1];
 
 #define BUTTON_PIN 39
+Button2 button;
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-int numberOfDevices;
 
 AsyncMqttClient mqttClient;
 TimerHandle_t mqttReconnectTimer;
 TimerHandle_t wifiReconnectTimer;
 
-unsigned long previousMillis = 0;   // Stores last time temperature was published
-const long interval = 10000;        // Interval at which to publish sensor readings
+int numberOfDevices;                // Number of devices found at boot time.
+unsigned long previousMillis = 0;   // Stores last time temperature was published.
+const long interval = 10000;        // Interval at which to publish sensor readings.
+DeviceAddress tempDeviceAddress;    // Holds current address while looping.
 
 void connectToWifi() {
-  Serial.println("Connecting to Wi-Fi...");
+  Serial.println("[WiFi  ] Connecting to Wi-Fi...");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
 void connectToMqtt() {
-  Serial.println("Connecting to MQTT...");
+  Serial.println("[MQTT  ] Connecting to MQTT...");
   mqttClient.connect();
 }
 
 void WiFiEvent(WiFiEvent_t event) {
-  Serial.printf("[WiFi Event] event: %d\n", event);
+  Serial.printf("[WiFi  ] Event code: %d\n", event);
   switch(event) {
     case SYSTEM_EVENT_STA_GOT_IP:
-      Serial.println("Wi-Fi connected.");
-      Serial.println("IP address: ");
+      Serial.println("[WiFi  ] Wi-Fi connected.");
+      Serial.print("[WiFi  ] IP address: ");
       Serial.println(WiFi.localIP());
       connectToMqtt();
       break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
-      Serial.println("Wi-Fi lost connection.");
+      Serial.println("[WiFi  ] Wi-Fi connection lost.");
+      ledAtom[0].setRGB(255,0,0); FastLED.show();
       xTimerStop(mqttReconnectTimer, 0); // Avoid reconnecting to MQTT while Wi-Fi is down.
       xTimerStart(wifiReconnectTimer, 0);
       break;
@@ -58,128 +61,205 @@ void WiFiEvent(WiFiEvent_t event) {
 }
 
 void onMqttConnect(bool sessionPresent) {
-  Serial.println("Connected to MQTT.");
-  Serial.print("Session present: ");
-  Serial.println(sessionPresent);
+  Serial.print("[MQTT  ] Connected to MQTT, Client ID: ");
+  Serial.println(mqttClient.getClientId());
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  Serial.println("Disconnected from MQTT.");
+  Serial.println("[MQTT  ] Disconnected from MQTT.");
+  ledAtom[0].setRGB(255,0,0); FastLED.show();
   if (WiFi.isConnected()) {
     xTimerStart(mqttReconnectTimer, 0);
   }
 }
 
-/*void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-  Serial.println("Subscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-  Serial.print("  qos: ");
-  Serial.println(qos);
-}
-void onMqttUnsubscribe(uint16_t packetId) {
-  Serial.println("Unsubscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}*/
-
 void onMqttPublish(uint16_t packetId) {
-  Serial.println("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
+  Serial.print("[MQTT  ] Publish acknowledged, packet ID=");
+  Serial.print(packetId);
+  Serial.println(".");
 }
 
-void loopDevices() {
+void searchDevices() {
 
-
-    
-    Serial.println("Searching for 1-wire devices...");
-
-
-
+  Serial.println("[Sensor] Searching for 1-wire devices...");
 
   // Grab a count of devices on the wire
-    numberOfDevices = sensors.getDeviceCount();
+  sensors.begin();
+  sensors.begin(); // For some reason, this only works when called twice!?
 
-    Serial.print("Found ");
-    Serial.print(numberOfDevices, DEC);
-    Serial.println(" devices.");
+  numberOfDevices = sensors.getDeviceCount();
+  int numberOfDS18 = sensors.getDS18Count();
 
+  Serial.print("[Sensor] Found ");
+  Serial.print(numberOfDevices, DEC);
+  Serial.print(" devices (of which ");
+  Serial.print(numberOfDS18, DEC);
+  Serial.println(" are DS18).");
 
-    // // Search for connected devices...
-    // uint8_t address[8];
-    // uint8_t count = 0;
+  // Loop through each device, print out address and set resolution...
+  for(int i=0;i<numberOfDevices; i++)
+  {
+    if(sensors.getAddress(tempDeviceAddress, i))
+    {
+      Serial.print("[Sensor] Found device ");
+      Serial.print(i, DEC);
+      Serial.print(" with address ");
+      printAddress(tempDeviceAddress);
+      Serial.println("."); 
 
-    // if (oneWire.search(address)) {
+      Serial.print("[Sensor] Setting resolution to ");
+      Serial.print(TEMPERATURE_PRECISION, DEC);
+      Serial.println(" bits.");
+      sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
       
-    //   do {
-    //     count++;
-    //     Serial.println("  {");
-    //     for (uint8_t i = 0; i < 8; i++)
-    //     {
-    //       if (address[i] < 0x10) Serial.print("0");
-    //       Serial.print(address[i], HEX);
-    //     }
-    //     Serial.println("...");
-    //   } while (oneWire.search(address));
-
-    //   Serial.println(" # ");
-    //   Serial.print("// nr devices found: ");
-    //   Serial.println(count);
-
-
-      
-    // } else {
-    //   Serial.println("No devices found.");
-    // }
+      Serial.print("[Sensor] Resolution currently set to: ");
+      Serial.print(sensors.getResolution(tempDeviceAddress), DEC); 
+      Serial.println();
+    }else{
+      Serial.print("[Sensor] Found ghost device at ");
+      Serial.print(i, DEC);
+      Serial.print(" but could not detect address (check power and cabling).");
+    }
+  }
     
 }
 
 void setup() {
- 
-  Serial.begin(115200);
 
-  connectToWifi();
+  FastLED.addLeds<NEOPIXEL, PIN_LEDATOM>(ledAtom, 1);
+  ledAtom[0].setRGB(255,0,0); FastLED.show();
+
+  Serial.begin(115200);
+  delay(50);
+  Serial.println();
+  Serial.println();
+
+  button.begin(BUTTON_PIN);
+  button.setLongClickTime(1000);
+  button.setDoubleClickTime(400);
+
+  Serial.println("[Button] DoubleClick Time: " + String(button.getDoubleClickTime()) + "ms");
+  Serial.println("[Button] Longpress Time: " + String(button.getLongClickTime()) + "ms");
+  
+  button.setClickHandler(click);
+  button.setLongClickDetectedHandler(longClickDetected);
+  button.setDoubleClickHandler(doubleClick);
+
+  // Search for devices...
+  searchDevices();
 
   mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
   wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
 
-  WiFi.onEvent(WiFiEvent);
-
   mqttClient.onConnect(onMqttConnect);
   mqttClient.onDisconnect(onMqttDisconnect);
-
   mqttClient.onPublish(onMqttPublish);
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
   //mqttClient.setCredentials("USER", "PASSW");
-   
-  // Start the DS18B20 sensor
-  sensors.begin();
-  loopDevices();
 
-  
+  WiFi.onEvent(WiFiEvent);
+  connectToWifi();
+
 }
 
 void loop() {
- 
-  // unsigned long currentMillis = millis();
-  // // Every X number of seconds (interval = 10 seconds) 
-  // // it publishes a new MQTT message
+  
+  button.loop();
 
-  // if (currentMillis - previousMillis >= interval) {
-  //   // Save the last time a new reading was published
-  //   previousMillis = currentMillis;
-  //   // New temperature readings
-  //   sensors.requestTemperatures(); 
-  //   // Temperature in Celsius degrees
-  //   temp = sensors.getTempCByIndex(0);
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
     
-  //   // Publish an MQTT message on topic esp32/ds18b20/temperature
-  //   uint16_t packetIdPub1 = mqttClient.publish(MQTT_PREFIX, 1, true, String(temp).c_str());                            
-  //   Serial.printf("Publishing on topic %s at QoS 1, packetId: ", MQTT_PREFIX);
-  //   Serial.println(packetIdPub1);
-  //   Serial.printf("Message: %.2f \n", sensors.getTempCByIndex(0));
+    previousMillis = currentMillis;
 
-  // }
+    readSensors();
+
+  }
 
 }
+
+void readSensors() {
+
+  ledAtom[0].setRGB(0,50,0); FastLED.show();
+
+  Serial.println("[Sensor] Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("[Sensor] Temperature  request complete.");
+
+  // Loop through each device and print out temperature data...
+  for(int i=0;i<numberOfDevices; i++)
+  {
+
+    // Search the wire for the address...
+    if(sensors.getAddress(tempDeviceAddress, i))
+    {
+
+      String strAddress = getAddress(tempDeviceAddress);
+
+      // Read the current temperature...
+      float tempC = sensors.getTempC(tempDeviceAddress);
+      
+      if(tempC == DEVICE_DISCONNECTED_C) 
+      {
+        Serial.println("[Sensor] Error: Could not read temperature data for " + strAddress + ".");
+      } else {
+      
+        // Log the address and reading...
+        Serial.println("[Sensor] Temperature for device " + strAddress + " is " + tempC + " C.");
+        
+        // Publish an MQTT message...
+        String topic = MQTT_PREFIX + strAddress + "/temp";
+        uint16_t packetIdPub = mqttClient.publish(topic.c_str(), 1, true, String(tempC).c_str());                            
+        Serial.printf("[MQTT  ] Publishing on topic %s, payload ", topic);
+        Serial.printf("%.2f, packet ID=", tempC);
+        Serial.print(packetIdPub);
+        Serial.println(".");
+        
+      }
+
+    } 
+
+  }
+
+  ledAtom[0].setRGB(0,0,50); FastLED.show();
+
+}
+
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+String getAddress(DeviceAddress deviceAddress)
+{
+  char address[17];
+  char buffer[3];
+  for (int i = 0; i < 8; i++) {
+    sprintf(buffer, "%02x", deviceAddress[i]);
+    address[(i*2)]=buffer[0];
+    address[(i*2)+1]=buffer[1];
+  }
+  address[16] = '\0';
+  return String(address);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void click(Button2& btn) {
+    Serial.println("[Button] Click");
+}
+
+void longClickDetected(Button2& btn) {
+    Serial.println("[Button] Long Click");
+    searchDevices();
+}
+
+void doubleClick(Button2& btn) {
+    Serial.println("[Button] Double Click");
+}
+
+
+/////////////////////////////////////////////////////////////////
--- src/Secrets-Example.h
+++ src/Secrets-Example.h
@@ -1,2 +1,6 @@
 #define WIFI_SSID "SSID"
 #define WIFI_PASSWORD "Password"
+
+#define MQTT_HOST "iot.preventx.net" // Host or IP address.
+#define MQTT_PORT 1883
+#define MQTT_PREFIX "px/temperature/"
